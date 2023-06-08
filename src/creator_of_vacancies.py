e7a472911fbf58f2e2eaaed0374557e8b125919d
Nikolai <kolamba.r@gmail.com>
2023-06-08 23:00:39 +0300

    creator of Vacancy's instance is done and tested. (*for superjob)

--- src/creator_of_vacancies.py
+++ src/creator_of_vacancies.py
@@ -15,26 +15,63 @@ class CreatorOfVacancies(ABC):
 class CreatorFromHh(CreatorOfVacancies):
 
     @staticmethod
-    def make_vacancy(job: dict):
-        vacancy_instances = []
-        vacancies = job['items']
+    def make_vacancy(job: dict) -> list:
+        vacancy_instances = []  # список для хранения экземпляров
+        vacancies = job['items']  # берет только информацию о вакансиях
+
         for vac in vacancies:
+
             vacancy = Vacancy()
-            vacancy.name = vac['name']
-            vacancy.place = vac['area']['name']
+
+            # каждую вакансию превращает в объект класса Vacancy, присваивая полям значения из словаря
+            vacancy.name = vac['name']   # название вакансии
+            vacancy.place = vac['area']['name']   # место работы территориально
             vacancy.salary_from = vac['salary']['from']
             vacancy.salary_to = vac['salary']['to']
             vacancy.salary_currency = vac['salary']['currency']
             vacancy.url = vac['alternate_url']
 
+            # тут нужно поменять значение, если оно None, на пустую строку
             if vac['snippet']['requirement'] == None:
                 vac['snippet']['requirement'] = ''
             if vac['snippet']['responsibility'] == None:
                 vac['snippet']['responsibility'] = ''
 
+            # требования и описания записывает в одно поле
             vacancy.description = vac['snippet']['requirement'] + '\n' + vac['snippet']['responsibility']
             vacancy.operating_mode = vac['employment']['name']
+
+            # оставляет метку, где была взята вакансия
             vacancy.made_from = 'HeadHunter'
+
+            # добавляет в экземпляр с заполненными полями в список и возвращает его
             vacancy_instances.append(vacancy)
         return vacancy_instances
 
+
+class CreatorFromJs(CreatorOfVacancies):
+
+    @staticmethod
+    def make_vacancy(job: dict) -> list:
+        vacancy_instances = []  # список для хранения экземпляров
+        vacancies = job['objects']  # берет только информацию о вакансиях
+
+        for vac in vacancies:
+            vacancy = Vacancy()
+
+            # каждую вакансию превращает в объект класса Vacancy, присваивая полям значения из словаря
+            vacancy.name = vac['profession']   # название вакансии
+            vacancy.place = vac['address']   # место работы территориально
+            vacancy.salary_from = vac['payment_from']
+            vacancy.salary_to = vac['payment_to']
+            vacancy.salary_currency = vac['currency']
+            vacancy.url = vac['link']
+            vacancy.description = vac['candidat']
+            vacancy.operating_mode = vac['type_of_work']['title']
+
+            # оставляет метку, где была взята вакансия
+            vacancy.made_from = 'SuperJob'
+
+            # добавляет в экземпляр с заполненными полями в список и возвращает его
+            vacancy_instances.append(vacancy)
+        return vacancy_instances
--- tests/test_creator_of_vacancies.py
+++ tests/test_creator_of_vacancies.py
@@ -1,7 +1,11 @@
-from src.creator_of_vacancies import CreatorFromHh
+from src.creator_of_vacancies import CreatorFromHh, CreatorFromJs
 from src.vacancy import Vacancy
 
 
 def test_make_vacancy(test_dict_hh):
     test_vacancy_instances = CreatorFromHh.make_vacancy(test_dict_hh)
+    assert type(test_vacancy_instances[0]) == Vacancy
+
+def test_make_vacancy_js(test_dict_sj):
+    test_vacancy_instances = CreatorFromJs.make_vacancy(test_dict_sj)
     assert type(test_vacancy_instances[0]) == Vacancy
