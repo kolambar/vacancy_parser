13e3e29348f0bcf72b745bacbae3cd957e0f5124
Nikolai <kolamba.r@gmail.com>
2023-06-16 13:08:02 +0300

    now user can choose profession before parsing

--- src/api_asker.py
+++ src/api_asker.py
@@ -8,7 +8,7 @@ class ApiAsker(ABC):
     абстрактный клас требующий метод для подключения по API и метод для получения вакансий
     """
     @abstractmethod
-    def get_vacancy(self):
+    def get_vacancy(self, profession):
         """
         метод для получения вакансий
         :return:
@@ -20,18 +20,23 @@ class HhApiAsker(ApiAsker):
     """
     подключается к HeadHunter
     """
-    def get_vacancy(self):
+    def get_vacancy(self, profession):
         """
         подключается к HeadHunter и получает вакансии
         """
-        return requests.get('https://api.hh.ru/vacancies/?per_page=100').json()
+        params = {
+            "per_page=100": 100,
+            "page": 1,
+            "text": profession
+        }
+        return requests.get('https://api.hh.ru/vacancies/', params=params).json()
 
 
 class SjApiAsker(ApiAsker):
     """
     подключается к Superjob
     """
-    def get_vacancy(self):
+    def get_vacancy(self, profession):
         """
         подключается к Superjob и получает вакансии
         """
@@ -40,5 +45,6 @@ class SjApiAsker(ApiAsker):
         params = {
             "count": 100,
             "page": 1,
+            "keyword": profession
         }
         return requests.get('https://api.superjob.ru/2.0/vacancies', params=params, headers=headers).json()
--- tests/test_api_asker.py
+++ tests/test_api_asker.py
@@ -3,11 +3,11 @@ from src.api_asker import HhApiAsker, SjApiAsker
 
 def test_get_vacancy():
     test = HhApiAsker()
-    inf = test.get_vacancy()
+    inf = test.get_vacancy('курьер')
     assert inf
     assert type(inf) is dict
 
     test = SjApiAsker()
-    inf = test.get_vacancy()
+    inf = test.get_vacancy('курьер')
     assert inf
     assert type(inf) is dict
